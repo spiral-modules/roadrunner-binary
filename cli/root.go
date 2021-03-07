ede3f6b355406f9d886bee17d306f30875c536a8
Valery Piashchynski <piashchynski.valery@gmail.com>
2021-03-07 19:13:51 +0300

    Move plugins declaration to the plugins.go

--- cli/root.go
+++ cli/root.go
@@ -12,21 +12,8 @@ import (
 	"github.com/spiral/errors"
 
 	goridgeRpc "github.com/spiral/goridge/v3/pkg/rpc"
-	"github.com/spiral/roadrunner/v2/plugins/gzip"
-	"github.com/spiral/roadrunner/v2/plugins/headers"
-	httpPlugin "github.com/spiral/roadrunner/v2/plugins/http"
-	"github.com/spiral/roadrunner/v2/plugins/informer"
-	"github.com/spiral/roadrunner/v2/plugins/logger"
-	"github.com/spiral/roadrunner/v2/plugins/metrics"
-	"github.com/spiral/roadrunner/v2/plugins/reload"
-	"github.com/spiral/roadrunner/v2/plugins/resetter"
+
 	rpcPlugin "github.com/spiral/roadrunner/v2/plugins/rpc"
-	"github.com/spiral/roadrunner/v2/plugins/server"
-	"github.com/spiral/roadrunner/v2/plugins/static"
-	"github.com/spiral/roadrunner/v2/plugins/status"
-	"github.com/temporalio/roadrunner-temporal/activity"
-	temporalClient "github.com/temporalio/roadrunner-temporal/client"
-	"github.com/temporalio/roadrunner-temporal/workflow"
 
 	"github.com/spiral/roadrunner/v2/plugins/config"
 
@@ -146,38 +133,17 @@ func init() {
 			log.Fatal(err)
 		}
 
-		err = Container.RegisterAll(
-			// logger plugin
-			&logger.ZapLogger{},
-			// metrics plugin
-			&metrics.Plugin{},
-			// http server plugin
-			&httpPlugin.Plugin{},
-			// reload plugin
-			&reload.Plugin{},
-			// informer plugin (./rr workers, ./rr workers -i)
-			&informer.Plugin{},
-			// resetter plugin (./rr reset)
-			&resetter.Plugin{},
-			// rpc plugin (workers, reset)
-			&rpcPlugin.Plugin{},
-			// server plugin (NewWorker, NewWorkerPool)
-			&server.Plugin{},
-
-			// static
-			&static.Plugin{},
-			// headers
-			&headers.Plugin{},
-			// checker
-			&status.Plugin{},
-			// gzip
-			&gzip.Plugin{},
-
-			// temporal plugins
-			&activity.Plugin{},
-			&workflow.Plugin{},
-			&temporalClient.Plugin{},
-
+		// register plugin from the plugins.go
+		for i := 0; i < len(plugins); i++ {
+			err = Container.Register(
+				// register plugin
+				plugins[i],
+			)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+		err = Container.Register(
 			// register config
 			cfg,
 		)
